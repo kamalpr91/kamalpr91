62568e934fc3105766fad50bca1cf7a0c727fb6e
kamalpr91 <105814851+kamalpr91@users.noreply.github.com>
2023-09-19 11:35:18 +0530

    Add files via upload

---
+++ Springboot_Paytm-Application-1/src/main/java/com/ionicx/SpringbootPaytmApplication1Application.java
@@ -0,0 +1,17 @@
+package com.ionicx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringbootPaytmApplication1Application {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPaytmApplication1Application.class, args);
+	}
+
+}
